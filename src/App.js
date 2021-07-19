d040916eb87458005b579eab9493065764f20059
Crystal Anderson <cryssann@gmail.com>
2021-07-19 17:02:40 -0600

    Display completed

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import Hero from './components/Hero'
+import { BrowserRouter as Router } from 'react-router-dom';
+import { GlobalStyle } from './globalStyles';
+import Products from './components/Products';
+import { productData, productDataTwo } from './components/Products/data'
+import Feature from './components/Feature'
+import Footer from './components/Footer'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+    <GlobalStyle />
+    <Hero />
+    <Products headings='Choose your favorite' data={productData} />
+    <Feature />
+    <Products headings='For Your Sweet Tooth' data={productDataTwo} />
+    <Footer />
+    </Router>
   );
 }
 
---
+++ src/components/Products/data.js
@@ -0,0 +1,81 @@
+import product1 from '../../images/product-1.jpeg';
+import product2 from '../../images/product-2.jpeg';
+import product3 from '../../images/product-3.jpeg';
+import product4 from '../../images/product-4.jpeg';
+import product5 from '../../images/product-5.jpeg';
+import product6 from '../../images/product-6.jpeg';
+import product7 from '../../images/product-7.jpeg';
+import product8 from '../../images/product-8.jpeg';
+
+
+export const productData = [
+    {
+        img: product1,
+        alt: 'Pizza',
+        name: 'Supreme Pizza',
+        desc: 'Marinara Sauce, Basil, Sausage, Tomatoes, Olives, Pesto',
+        price: '$19.99',
+        button: 'Add to Cart'
+    },
+    {
+        img: product2,
+        alt: 'Pizza',
+        name: 'Pepperoni Pizza',
+        desc: 'Marinara Sauce, Cheese, Pepperoni',
+        price: '$12.99',
+        button: 'Add to Cart'
+    },
+    {
+        img: product3,
+        alt: 'Pizza',
+        name: 'Cheese Pizza',
+        desc: 'Marinara Sauce, Cheese',
+        price: '$10.99',
+        button: 'Add to Cart'
+    },
+    {
+        img: product4,
+        alt: 'Pizza',
+        name: 'Barbecue Pizza',
+        desc: 'Barbecue Sauce, Chicken, Cheese',
+        price: '$15.99',
+        button: 'Add to Cart'
+    },
+
+]
+
+export const productDataTwo = [
+    {
+        img: product5,
+        alt: 'Dessert',
+        name: 'Icecream',
+        desc: 'Vanilla icecream, brownie, chocolate & caramel drizzle',
+        price: '$10.99',
+        button: 'Add to Cart'
+    },
+    {
+        img: product6,
+        alt: 'Dessert',
+        name: 'Raspberry Icecream',
+        desc: 'Raspberry icecream bar',
+        price: '$8.99',
+        button: 'Add to Cart'
+    },
+    {
+        img: product7,
+        alt: 'Dessert',
+        name: 'Orero Cupcake',
+        desc: 'Oreos dropped on chocolate cupcake',
+        price: '$6.99',
+        button: 'Add to Cart'
+    },
+    {
+        img: product8,
+        alt: 'Dessert',
+        name: 'Cheesecake',
+        desc: 'Cheesecake topped with raspberries, blueberries, lemons, mint',
+        price: '$18.99',
+        button: 'Add to Cart'
+    },
+
+]
